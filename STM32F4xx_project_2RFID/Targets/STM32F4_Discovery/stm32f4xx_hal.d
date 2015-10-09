81e7af6f82adda639ab471c288b0b654b13c16a5
Trần Nhật Đức <wsanlab411@gmail.com>
2015-10-09 09:07:00 +0700

    Update
    project 2 RFID update

---
+++ STM32F4xx_project_2RFID/Targets/STM32F4_Discovery/stm32f4xx_hal.d
@@ -0,0 +1,2 @@
+.\targets\stm32f4_discovery\stm32f4xx_hal.o: ..\00-STM32F4xx_HAL_DRIVERS\STM32F4xx_HAL_Driver\Src\stm32f4xx_hal.c
+.\targets\stm32f4_discovery\stm32f4xx_hal.o: ..\00-STM32F4xx_HAL_DRIVERS\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal.h
---
+++ STM32F4xx_project_2RFID/Targets/STM32F4_Discovery/syscall.d
@@ -0,0 +1,5 @@
+.\targets\stm32f4_discovery\syscall.o: ..\00-STM32F429_LIBRARIES\fatfs\option\syscall.c
+.\targets\stm32f4_discovery\syscall.o: ..\00-STM32F429_LIBRARIES\fatfs\option\../ff.h
+.\targets\stm32f4_discovery\syscall.o: ..\00-STM32F429_LIBRARIES\fatfs\option\../integer.h
+.\targets\stm32f4_discovery\syscall.o: ..\00-STM32F429_LIBRARIES\fatfs\option\../ffconf.h
+.\targets\stm32f4_discovery\syscall.o: E:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
